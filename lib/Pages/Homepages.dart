6a52acd266b4dcc39e8b251d901016c5730f7e77
anuteshome <ananyateshome2@gmail.com>
2026-03-04 00:12:24 +0300

    feat: creating Homepage

---
+++ lib/Pages/Homepages.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Homepages extends StatelessWidget {
+  const Homepages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterwidgets/Pages/Homepages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body:Center(
-child: Text("name"),
-        ),
-      ),
+      home: Homepages(),
     );
   }
 }
