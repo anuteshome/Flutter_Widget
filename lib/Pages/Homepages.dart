22d551984b4f5fae1ad8edf82684f0f993a2d8a8
anuteshome <ananyateshome2@gmail.com>
2026-03-04 00:17:11 +0300

    feat: making a container

--- lib/Pages/Homepages.dart
+++ lib/Pages/Homepages.dart
@@ -8,7 +8,13 @@ class Homepages extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Container(
-          
+          height: 200,
+          width: 200,
+        
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+              color: Colors.deepPurpleAccent,
+          ),
         ),
       ),
     );
